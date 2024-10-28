cbc6be1483fcd8b94cde4e0e7a40f4b7b9c7ce78
shakeyiab <shakeyiabartell@gmail.com>
2024-10-28 19:15:12 -0400

    customize header and footers

---
+++ app.mjs
@@ -0,0 +1,66 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}function Header() {
+  return (<h1>Simple React Application</h1>);
+}
+
+function Content(props) {
+  return (<p style={{color: props.color}}>{props.text}</p>);
+}
+
+function Footer() {
+  return (<h1>Created by Me, of course.</h1>);
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+    </>
+  );
+}
+function Header() {
+    return (<h1>Simple React Application</h1>);
+  }
+  
+  function Content() {
+    return (<p>of course i broke it after came working </p>);
+  }
+  
+  function Footer() {
+    return (<h1>Created by Me, of course its still broken .</h1>);
+  }
+
+  function App() {
+    return (
+      <>
+  
+      </>
+    );
+  }
+
+export default App;
