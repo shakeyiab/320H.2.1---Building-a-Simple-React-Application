7031013ba1dc3b0f40b5e551d5efa29129176d19
shakeyiab <shakeyiabartell@gmail.com>
2024-10-28 19:16:55 -0400

    Adding style

--- app.mjs
+++ app.mjs
@@ -66,7 +66,9 @@ function Header() {
     return (
       <>
         <Header />
-        <Content />
+        <Content color="blue" text="This is my first React Application!" />
+        <Content color="red" text="Wish me luck..." />
+        <Content color="green" text="I think I've got it!" />
         <Footer />
       </>
     );
